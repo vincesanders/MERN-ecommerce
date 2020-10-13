16a396f61d7a2f324c59fcac27cb68eb8ddf1bd9
Vincent Sanders <vincesanders@hotmail.com>
2020-10-13 13:05:34 -0500

    Added functionality to server to handle environmetal variables.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
+
+dotenv.config();
 
 const app = express();
 
@@ -16,4 +19,7 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product);
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+const ENV = process.env.NODE_ENV
+
+app.listen(5000, console.log(`Server running in ${ENV} mode on port ${PORT}`));
