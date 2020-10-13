54014551d6e3a96b20e13a6dee04f87a55b89cbc
Vincent Sanders <vincesanders@hotmail.com>
2020-10-13 09:08:15 -0500

    Integrated bootstrap and FontAwesome.
    Created scaffolding for NavBar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>RuneSpeakers Edda Shop</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
