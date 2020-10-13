2de75abb1f3c22692042cdf511dab717e90f717d
Vincent Sanders <vincesanders@hotmail.com>
2020-10-13 12:05:01 -0500

    Initial scaffolding of backend.

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -15,40 +15,26 @@ const ProductScreen = ({ match }) => {
                 </Col>
                 <Col md={3}>
                     <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <h3>{product.name}</h3>
-                        </ListGroup.Item>
+                        <ListGroup.Item><h3>{product.name}</h3></ListGroup.Item>
                         <ListGroup.Item>
                             <Rating value={product.rating} text={`${product.numReviews} reviews`} />
                         </ListGroup.Item>
-                        <ListGroup.Item>
-                            Price: ${product.price}
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Description: {product.description}
-                        </ListGroup.Item>
+                        <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                        <ListGroup.Item>Description: {product.description}</ListGroup.Item>
                     </ListGroup>
                 </Col>
                 <Col md={3}>
                     <ListGroup variant='flush'>
                         <ListGroup.Item>
                             <Row>
-                                <Col>
-                                Price: 
-                                </Col>
-                                <Col>
-                                    <strong>{product.price}</strong>
-                                </Col>
+                                <Col>Price:</Col>
+                                <Col><strong>{product.price}</strong></Col>
                             </Row>
                         </ListGroup.Item>
                         <ListGroup.Item>
                             <Row>
-                                <Col>
-                                Status: 
-                                </Col>
-                                <Col>
-                                    {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                                </Col>
+                                <Col>Status:</Col>
+                                <Col>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</Col>
                             </Row>
                         </ListGroup.Item>
                         <ListGroup.Item>
