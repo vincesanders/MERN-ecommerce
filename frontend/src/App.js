c920c92527dae176621f7ceada0d2c1782d1ab8e
Vincent Sanders <vincesanders@hotmail.com>
2020-10-13 09:53:04 -0500

    Added HomeScreen and Product component.
    Also added images to public folder.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
     <div>
       <Header />
-      <main className='py-3'>
-        <Container>
-          <h1>RuneSpeakers Edda Shop</h1>
-        </Container>
-      </main>
+        <main className='py-3'>
+          <Container>
+            <HomeScreen />
+          </Container>
+        </main>
       <Footer />
     </div>
   );
